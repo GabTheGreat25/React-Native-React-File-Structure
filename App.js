7b576b4c195b56c64841d572e1e29cbd10303aa1
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-09-10 01:14:30 +0800

    Change styles to nativewind

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import React from "react";
 import { SafeAreaView, ScrollView } from "react-native";
 import { PokemonCard } from "@/components";
 import { PokemonData } from "@/data";
 import { appStyles } from "@/styles";
 
 const App = () => (
-  <SafeAreaView style={appStyles.container}>
+  <SafeAreaView
+    className={`flex-1 bg-primary-default ${
+      Platform.OS === "android" ? "pt-6" : "pt-0"
+    }`}
+  >
     <ScrollView>
       {PokemonData.map((pokemon, index) => {
         console.log("Pokemon index:", pokemon);
