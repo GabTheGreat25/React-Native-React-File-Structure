ab4db2180254a80da9e088fe40cfbc358c3d7698
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-09-14 12:44:14 +0800

    Center the ui

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default function App() {
   const { colorScheme, toggleColorScheme } = useColorScheme();
   return (
     <View className="items-center justify-center flex-1 bg-light-default dark:bg-dark-default">
-      <View className="flex-row w-full gap-5">
+      <View className="flex items-center justify-center w-full gap-5">
         <Switch value={colorScheme === "dark"} onChange={toggleColorScheme} />
         <Text selectable={false} className="dark:text-white">
           {`Try clicking me! ${colorScheme === "dark" ? "🌙" : "🌞"}`}
