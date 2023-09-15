08a6abd6595dba47b8c5d5a22f082b666ebaf978
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-09-15 21:09:37 +0800

    Fix darkmode

--- App.js
+++ App.js
@@ -3,11 +3,21 @@ import { useColorScheme } from "nativewind";
 
 export default function App() {
   const { colorScheme, toggleColorScheme } = useColorScheme();
+
   return (
-    <View className="items-center justify-center flex-1 bg-light-default dark:bg-dark-default">
+    <View
+      className={`items-center justify-center flex-1 ${
+        colorScheme === "dark" ? "bg-dark-default" : "bg-light-default"
+      }`}
+    >
       <View className="flex items-center justify-center w-full gap-5">
         <Switch value={colorScheme === "dark"} onChange={toggleColorScheme} />
-        <Text selectable={false} className="dark:text-white">
+        <Text
+          selectable={false}
+          className={`${
+            colorScheme === "dark" ? "text-light-default" : "text-dark-default"
+          }`}
+        >
           {`Try clicking me! ${colorScheme === "dark" ? "🌙" : "🌞"}`}
         </Text>
       </View>
